98687bc50226525b0a1663026237a832d45b1047
Hugo De la Rosa <hugo@quopera.com>
2024-12-03 09:37:45 -0800

    fix(lint): errors fixing

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,14 +1,17 @@
 import {fetchCatalog} from "@/app/lib/products";
-import {CategoryCollection} from "@/app/lib/definitions";
 import Filters from "@/app/ui/catalog/category/Filters";
 import {fetchCategoryName} from "@/app/lib/data";
 
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const catalog: CategoryCollection = await fetchCatalog(params.id);
+  const id = params.id;
+  const [catalog, category_name] = await Promise.all([
+    fetchCatalog(id),
+    fetchCategoryName(id),
+  ]);
   const products = catalog.products;
   const filters = catalog.filters;
-  const category_name = await fetchCategoryName(params.id);
+
 
   return (
     <Filters filters={filters} products={products} category_name={category_name}/>
